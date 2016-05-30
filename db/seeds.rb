38c81b97dfb24f607aa1008eb0d3196a13872598
Leonam Anjos <leonam.ricardo@gmail.com>
2016-05-30 14:34:19 -0300

    Change index layout to show cpu of each process

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-[ {id: '1', hostname: 'host1', report: { cpu: 12.5, disk: 19.4, process: %w{/procA /procB} }},
-  {id: '2', hostname: 'host2', report: { cpu: 2.5, disk: 39.2, process: %w{/procC} } },
-  {id: '3', hostname: 'host3', report: { cpu: 1.4, disk: 32.1, process: %w{/procD /procE /procF} } },
-  {id: '4', hostname: 'host4', report: { cpu: 51.7, disk: 1.4, process: %w{/procG /procH} } },
-  {id: '5', hostname: 'host5', report: { cpu: 92.5, disk: 79.5, process: %w{/procI /procJ /procK /procL} } }
+[ {id: '1', hostname: 'host1', report: { cpu: 12.5, disk: 19.4, process: [ %w{/procA 2.6}, %w{/procB 5.6} ] }},
+  {id: '2', hostname: 'host2', report: { cpu: 2.5, disk: 39.2, process: [%w{/procC 3.3}] } },
+  {id: '3', hostname: 'host3', report: { cpu: 1.4, disk: 32.1, process: [%w{/procD 3.5}, %w{/procE 5.4}, %w{/procF 6.2}] } },
+  {id: '4', hostname: 'host4', report: { cpu: 51.7, disk: 1.4, process: [%w{/procG 3.1}, %w{/procH 2.2}] } },
+  {id: '5', hostname: 'host5', report: { cpu: 92.5, disk: 79.5, process: [%w{/procI 9.1}, %w{/procJ 3.1}, %w{/procK 3.3}, %w{/procL 5.6}] } }
 ].each { |info| CpuInfo.create(info) }
 
 
